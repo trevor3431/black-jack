a3eaefbc61885df7fad2f89316d04b7e812d2131
Trevor Ward <38332651+trevor3431@users.noreply.github.com>
2018-04-26 16:53:19 -0400

    Add files via upload
    Added betting functionality

--- blackjack.rb
+++ blackjack.rb
@@ -22,6 +22,8 @@ class Blackjack
     hit_stand
   end
 
+
+
   def hit_stand
     print "\nWhat would you like to do: \n\t1:> Hit\n\t2:> Stand\n > "
     @response = gets.chomp.to_i
@@ -62,6 +64,9 @@ class Blackjack
 end
 
 class Dealer < Blackjack
+  def initialize
+    @@chips = 500
+  end
   def hand()
     @cards = ['A', 'K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2']
     @card_value = {
@@ -100,10 +105,19 @@ class Dealer < Blackjack
 end
 
 class Engine
-@@player_score = 0
-@@dealer_score = 0
+  @@player_score = 0
+  @@dealer_score = 0
+  @@chips = 500
+  def wager
+    puts "You have $#{@@chips}"
+    puts "What would you like to wager?"
+    print ":> $ "
+    @@wager = $stdin.gets.chomp.to_i
+  end
+
   def play()
   player = Blackjack.new
+  wager
   dealer = Dealer.new
   puts "Dealer"
   dealer.hand
@@ -121,12 +135,18 @@ class Engine
   puts "\nDealer:> #{dealer.value}\n\n"
     if dealer.value > player.value && dealer.value <= 21 || player.value > 21
       puts "<<<DEALER WINS>>>\n"
+      @@chips -= @@wager
+      puts "You lost $#{@@wager}"
+      puts "Current balance /> $#{@@chips}"
       puts "SCORE"
       30.times{print "="}
       @@dealer_score += 1
       puts "\nPlayer: #{@@player_score} Dealer: #{@@dealer_score}"
     elsif dealer.value < player.value && player.value <= 21 || dealer.value > 21 && player.value <= 21
       puts "<<<PLAYER WINS>>>\n"
+      @@chips += @@wager
+      puts "You won $#{@@wager}"
+      puts "Current balance /> $#{@@chips}"
       @@player_score += 1
       puts "SCORE"
       30.times{print "="}
